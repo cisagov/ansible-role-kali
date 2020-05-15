08682bb0a859944349f00d79056bea3beb13d557
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2020-05-15 17:30:14 -0400

    Remove unused variables and correct tag values

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -10,36 +10,12 @@ variable "aws_region" {
   default     = "us-east-1"
 }
 
-variable "production_bucket_name" {
-  type        = string
-  description = "The name of the S3 bucket where the production Cobalt Strike tarball and license live."
-  default     = "cisa-cool-third-party-production"
-}
-
-variable "production_objects" {
-  type        = list(string)
-  description = "The Cobalt Strike tarball and license objects inside the production bucket."
-  default     = ["cobaltstrike.tgz", "cobaltstrike.license"]
-}
-
-variable "staging_bucket_name" {
-  type        = string
-  description = "The name of the S3 bucket where the staging Cobalt Strike tarball and license live."
-  default     = "cisa-cool-third-party-staging"
-}
-
-variable "staging_objects" {
-  type        = list(string)
-  description = "The Cobalt Strike tarball and license objects inside the staging bucket."
-  default     = ["cobaltstrike.tgz", "cobaltstrike.license"]
-}
-
 variable "tags" {
   type        = map(string)
   description = "Tags to apply to all AWS resources created"
 
   default = {
-    Team        = "NCATS OIS - Development"
-    Application = "ansible-role-cobalt-strike testing"
+    Team        = "VM Fusion - Development"
+    Application = "ansible-role-kali testing"
   }
 }
